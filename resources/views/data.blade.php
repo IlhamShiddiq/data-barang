7ce8df5e467ef9fb37f6b989e086a44e882b3035
IlhamShiddiq <shdqillham123@gmail.com>
2020-09-13 23:20:17 +0700

    detail modal

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -28,8 +28,8 @@
                     <th scope="row">1</th>
                     <td>Mark</td>
                     <td>
-                        <a href="#" class="badge badge-info">Detail</a>
-                        <a href="#" class="badge badge-success">Edit</a>
+                        <a href="#" class="badge badge-info" data-toggle="modal" data-target="#detail">Detail</a>
+                        <a href="edit" class="badge badge-success">Edit</a>
                         <a href="#" class="badge badge-danger">Delete</a>
                     </td>
                   </tr>
@@ -55,4 +55,54 @@
               </table>
         </div>
     </div>
+@endsection
+
+@section('more')
+  <!-- Logout Modal -->
+  <div class="modal fade" id="detail" tabindex="-1" aria-labelledby="Detail Modal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Nama ( Id )</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-4 text-center">
+                    <div class="view-img">
+
+                    </div>
+                  </div>
+                  <div class="col-md-8">
+                    <form>
+                      <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="basic-addon1">Cat</span>
+                        </div>
+                        <input type="text" class="form-control form-control-sm" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                      </div>
+                      <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="basic-addon1">Exp</span>
+                        </div>
+                        <input type="text" class="form-control form-control-sm" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                      </div>
+                      <div class="input-group mb-1">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="basic-addon1">Stk</span>
+                        </div>
+                        <input type="text" class="form-control form-control-sm" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer"> </div>
+        </div>
+    </div>
+  </div>
 @endsection
