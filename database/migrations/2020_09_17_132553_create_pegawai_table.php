90b085875ad4a69561d677be37f4969bd229df4c
IlhamShiddiq <shdqillham123@gmail.com>
2020-09-17 20:59:53 +0700

    add database migration

--- database/migrations/2020_09_17_132553_create_pegawai_table.php
+++ database/migrations/2020_09_17_132553_create_pegawai_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePegawaiTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pegawai', function (Blueprint $table) {
             $table->id('id');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('username');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nama');
+            $table->text('alamat');
+            $table->string('phone');
+            $table->date('tgl_lahir');
+            $table->string('image');
             $table->timestamps();
+            $table->foreign('id')->references('id')->on('users');
         });
     }
 
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pegawai');
     }
 }
---
+++ database/migrations/2020_09_17_132618_create_barang_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_kategori');
+            $table->string('barang');
+            $table->integer('stok');
+            $table->date('expired');
+            $table->string('image');
+            $table->timestamps();
+            $table->foreign('id_kategori')->references('id')->on('kategori');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
